6d8c53bffef90a19d25153c918c7c145c6c69aee
1232 <1232@DESKTOP-J9EK8AF>
2023-05-25 21:36:21 -0500

    Agregar archivos de proyecto.

---
+++ Graph.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller_Grafos_Kenier
+{
+    class Graph
+    {
+    private int[,] adjacencyMatrix;
+    private int numVertices;
+
+    public void CreateGraph(int numVertices, int[,] edges)
+    {
+        this.numVertices = numVertices;
+        adjacencyMatrix = new int[numVertices, numVertices];
+
+        for (int i = 0; i < edges.GetLength(0); i++)
+        {
+            int source = edges[i, 0];
+            int destination = edges[i, 1];
+            AddEdge(source, destination);
+        }
+    }
+
+    public void AddEdge(int source, int destination)
+    {
+        adjacencyMatrix[source, destination] = 1;
+        adjacencyMatrix[destination, source] = 1;
+    }
+
+    public void PrintGraph()
+    {
+        Console.WriteLine("Adjacency Matrix:");
+        for (int i = 0; i < numVertices; i++)
+        {
+            for (int j = 0; j < numVertices; j++)
+            {
+                Console.Write(adjacencyMatrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public List<int> FindPath(int start, int end)
+    {
+        Dictionary<int, int> visited = new Dictionary<int, int>();
+        Queue<int> queue = new Queue<int>();
+        Dictionary<int, int> parent = new Dictionary<int, int>();
+        List<int> path = new List<int>();
+
+        visited[start] = 1;
+        queue.Enqueue(start);
+
+        while (queue.Count > 0)
+        {
+            int current = queue.Dequeue();
+
+            if (current == end)
+            {
+                int node = end;
+                while (node != start)
+                {
+                    path.Insert(0, node);
+                    node = parent[node];
+                }
+                path.Insert(0, start);
+                break;
+            }
+
+            for (int i = 0; i < numVertices; i++)
+            {
+                if (adjacencyMatrix[current, i] == 1 && !visited.ContainsKey(i))
+                {
+                    visited[i] = 1;
+                    queue.Enqueue(i);
+                    parent[i] = current;
+                }
+            }
+        }
+
+        return path;
+    }
+  }
+
+}
+
+
+    
---
+++ Program.cs
@@ -0,0 +1,108 @@
+﻿using Taller_Grafos_Kenier;
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        Graph graph = new Graph();
+
+        Console.Write("Ingrese el número de vértices: ");
+        int numVertices = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Ingrese el número de aristas: ");
+        int numEdges = Convert.ToInt32(Console.ReadLine());
+
+        int[,] edges = new int[numEdges, 2];
+
+        for (int i = 0; i < numEdges; i++)
+        {
+            Console.WriteLine("Ingrese la arista " + (i + 1) + ":");
+            Console.Write("Vértice fuente: ");
+            int source = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Vértice destino: ");
+            int destination = Convert.ToInt32(Console.ReadLine());
+
+            edges[i, 0] = source;
+            edges[i, 1] = destination;
+        }
+
+        graph.CreateGraph(numVertices, edges);
+
+        Menu(graph);
+    }
+
+    static void Menu(Graph graph)
+    {
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.WriteLine("----- Menú -----");
+            Console.WriteLine("1. Visualizar Grafo");
+            Console.WriteLine("2. Agregar Nodo al Grafo");
+            Console.WriteLine("3. Buscar Camino entre Nodos");
+            Console.WriteLine("4. Salir");
+            Console.Write("Seleccione una opción: ");
+
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Se muestra el grafo :) ");
+                    graph.PrintGraph();
+                    Console.WriteLine();
+                    break;
+                case 2:
+                    Console.WriteLine("Agregar un nodo al grafo:");
+                    Console.Write("Vértice fuente: ");
+                    int newNodeSource = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Vértice destino: ");
+                    int newNodeDestination = Convert.ToInt32(Console.ReadLine());
+
+                    graph.AddEdge(newNodeSource, newNodeDestination);
+
+                    Console.WriteLine("Nuevo Grafo:");
+                    graph.PrintGraph();
+                    Console.WriteLine();
+                    break;
+                case 3:
+                    Console.WriteLine("Buscar camino entre nodos:");
+                    Console.Write("Vértice inicio: ");
+                    int startNode = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Vértice fin: ");
+                    int endNode = Convert.ToInt32(Console.ReadLine());
+
+                    List<int> path = graph.FindPath(startNode, endNode);
+
+                    Console.WriteLine("Camino desde el nodo " + startNode + " al nodo " + endNode + ":");
+                    if (path.Count == 0)
+                    {
+                        Console.WriteLine("No se encontró un camino.");
+                    }
+                    else
+                    {
+                        foreach (int node in path)
+                        {
+                            Console.Write(node + " ");
+                        }
+                    }
+
+                    Console.WriteLine();
+                    break;
+                case 4:
+                    exit = true;
+                    break;
+                default:
+                    Console.WriteLine("Deja la bobada que esa no es una opcion del menú. Gracias :)");
+                    break;
+            }
+        }
+    }
+}
+
+
